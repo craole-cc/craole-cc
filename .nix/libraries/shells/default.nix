2f57c19ee2c1d7a9746bc4dbb364b6bf177b0214
Craole <32288735+Craole@users.noreply.github.com>
2026-05-04 02:10:50 -0500

    shells still not working with scripts

--- .nix/libraries/shells/default.nix
+++ .nix/libraries/shells/default.nix
@@ -13,13 +13,10 @@
   lib,
   paths ? {},
   ...
-}: let
-  inherited = lib.shells or {};
-in
-  lib.assembly.assemble {
-    start = inherited // {inherit paths;};
-    entries = ./.;
-    scope = acc: lib // {shells = inherited // acc;};
-    priority = ["scripts.nix" "tools.nix"];
-    extraArgs = {inherit paths;};
-  }
+}:
+lib.assembly.importLibs {
+  inherit lib;
+  path = ./.;
+  priority = ["scripts.nix" "tools.nix"];
+  args = {inherit paths;};
+}
