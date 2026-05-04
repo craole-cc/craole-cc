dc327cc53f4e26ac5ee808c20922f4778216e056
Craole <32288735+Craole@users.noreply.github.com>
2026-05-04 01:50:31 -0500

    flattened shells.core

--- .nix/libraries/shells/default.nix
+++ .nix/libraries/shells/default.nix
@@ -1,5 +1,15 @@
-{lib}:
-lib.assembly.importLibs {
-  inherit lib;
-  path = ./.;
-}
+{
+  lib,
+  paths ? {},
+  ...
+}: let
+  inherit (lib.assembly) assemble;
+  inherited = lib.shells or {};
+in
+  assemble {
+    start = inherited;
+    entries = ./.;
+    scope = acc: lib // {shells = inherited // acc;};
+    priority = ["scripts.nix" "tools.nix"];
+    extraArgs = {inherit lib paths;};
+  }
